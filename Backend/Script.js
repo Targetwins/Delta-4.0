50a01494201745adc5ba62d01d2a4a99c63a77b4
Targetwins <t.winstech01@gmail.com>
2024-04-17 21:50:08 +0530

    node strated

---
+++ Backend/Script.js
@@ -0,0 +1,8 @@
+/*for(let i=1;i<=4;i++){
+    console.log("outer loop");
+       for(let j=1;j<=3;j++){
+       console.log("Hello World!",j);
+       }
+   }
+   */
+  console.log("hello World");
--- JavaScript/app (4).js
+++ JavaScript/app (4).js
@@ -1,2 +1,105 @@
+/*
 //JavaScript(Part - 7)
+
+//this Keyword
+const student ={
+    name:"Girsh B S",
+    class:"12th",
+    math:98,
+    phy:91,
+    chemi:90,
+    getAvg() {
+        console.log(this)
+        let avg =(this.math+this.phy+this.chemi)/3;
+        console.log(`${this.name} get avg score is ${avg}`);
+    }
+};
+*/
+/*
+//Try & Catch
+console.log("Hello1");
+console.log("Hello2");
+console.log("Hello3");
+try{
+    console.log(a);
+}catch (err){
+    console.log("it is not defined");
+    console.log(err);
+
+}
+console.log('hi joke');
+
+*/
+/*
+//Arrow Function
+
+const sum = (a,b) =>{
+    return a-b;
+}
+console.log(sum);
+const hello = () =>{
+    return "hello world !"
+}
+console.log(hello());
+*/
+
+/*
+//implict return value
+const add = (a,b) => a+b;
+const div = (a,b) => a/b;
+const mod = (a,b) => a%b;
+
+*/
+/*
+// set Time function
+
+console.log("Hi There !");
+
+setTimeout(()=>{
+    console.log("upscMaga.com")
+},5000)
+
+    console.log("Welcome To")
+*/
+/*
+//set Interval
+
+console.log("Hi There !");
+
+id = setInterval(()=>{
+    console.log("UpscMAGA.com")
+},5000)
+
+id1 = setInterval(()=>{
+    console.log("Let's Strat Your Journey")
+},5000)
+*/
+/*
+//this keyword with arrow function
+
+const student = {
+    name:"Arjun",
+    age:12,
+    prop: this,
+    getName:function(){
+        return this.name;
+    },
+    getAge:() => {
+        return this.age;
+    }
+}
+
+*/
+/*
+//Pratice Questions
+
+//write function to print "Hello World !"5 times in each 2 seconds;
+ id = setInterval(() =>{
+    console.log("hello World !");
+},2000)
+ setTimeout(()=>{clearInterval(id);
+    console.log("check OUT !");
+ },10000)
+ */
+
 
